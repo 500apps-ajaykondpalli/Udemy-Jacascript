2cdf5bdccb533d0a38330cb02b673a571f6db52a
500apps-ajaykondpalli <85106783+500apps-ajaykondpalli@users.noreply.github.com>
2021-06-01 23:09:28 +0530

    Update myscript.js

--- 3.Javascript-fundamentals-part2/myscript.js
+++ 3.Javascript-fundamentals-part2/myscript.js
@@ -86,9 +86,9 @@ console.log(scoreDolphins, scoreKoalas);
 
 const checkWinner = function (avgDolphins, avgKoalas) {
     if (avgDolphins >= 2 * avgKoalas) {
-        console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
     } else if (avgKoalas >= 2 * avgDolphins) {
-        console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
     } else {
         console.log('No team wins...');
     }
